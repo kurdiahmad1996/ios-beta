a99620e7b8bba701a5370c97b144c9e18b1be0b7
kurdiahmad1996 <a.kurdi@livaatverse.com>
2022-11-22 12:47:28 +0300

    first commit

---
+++ iosbeta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosbeta
+//
+//  Created by Kurdi on 22/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosbeta/iosbetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosbetaApp.swift
+//  iosbeta
+//
+//  Created by Kurdi on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iosbetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
